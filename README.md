fb4fea314d1653a32c3a730ac0a9aecf3bc41492
Mr. kuldeep <kkharoliya20@gmail.com>
2024-05-26 12:15:32 +0530

    first upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Back to react js again...
+
+- download node js
+
+- creating react app with "vite" bundler 
+
+- command is
+
+```
+ npm create vite@latest
+ ```
+
+- follow the procedure of selecting options like : project name, technology, language etc.
+
+- change directory to our project file
+
+- run the command to install dependencies
+
+
+```
+npm install
+``` 
+
+- and all set! 😉
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+
+  return (
+    <>
+    <center>
+    <h1>Back to React js learning again... | Kuldeep kumar</h1>
+    </center>
+    <b>
+      <li>download node </li>
+      <li>creating react app with "vite" bundler </li>
+      <li> command is : npm create vite@latest</li>
+      <li>follow the procedure of selecting options like : project name, technology, language etc.</li>
+      <li>change directory to our project file</li>
+      <li>run the command "npm install " to install dependencies</li>
+      <li>and all set! 😉</li>
+    </b>
+    </>
+  )
+}
+
+export default App
